479c4207f84ec758a603533ff3a049c746b6a663
abii023 <abinputhanpurackal023@gmail.com>
2021-07-11 23:21:23 +0530

    insta bottam appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -8,40 +8,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
-
-class name extends StatelessWidget {
-  const name({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                icon: Icon(Icons.home)),
+            Spacer(),
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            Spacer(),
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+            Spacer(),
+            IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+            Spacer(),
+            IconButton(onPressed: () {}, icon: Icon(Icons.account_box_rounded)),
+            Spacer(),
+          ],
+        ),
+      ),
     );
   }
 }
